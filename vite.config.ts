0db630d1f452709fe07bb553890781acf0754b41
VitaliKozhurov <vitalik24021992@gmail.com>
2023-10-24 08:54:50 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import * as path from 'path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  server: {
+    open: true,
+    port: 3000,
+  },
+})
